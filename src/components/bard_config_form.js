b91629199b8620e9956777353ee6c12ce4b4ded3
Solomon Guo <beconegood@gmail.com>
2019-10-04 21:02:07 +0800

    完成基本功能

--- src/components/bard_config_form.js
+++ src/components/bard_config_form.js
@@ -1,32 +1,167 @@
 import React from 'react'
-import { Form, Tooltip, Icon, Input, Button, Row, Col } from 'antd'
+import { Form, Tooltip, Icon, Input, Button, Row, Col, Select, InputNumber, message } from 'antd'
 import yaml from 'js-yaml'
+import { api_host } from '../config'
+import { async } from 'q'
 
 const { Item } = Form
+const { Option } = Select
 
 class BardConfigForm extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            config: null
+            config: null,
+            plugins_list: null,
         }
     }
 
     componentDidMount() {
+        this.loadingPlugins()
         this.loading()
     }
 
     loading = async () => {
-        const response = await fetch('http://localhost:2019/bard/config/get')
+        const response = await fetch(api_host + '/bard/config/get')
         const data = await response.text();
+        // console.log("收到的config:", data)
 
         const config = yaml.safeLoad(data)
-        console.log(config)
+        // console.log(config)
         this.setState({ config })
     }
 
-    
+    loadingPlugins = async () => {
+        const response = await fetch(api_host + '/plugins/get')
+        const data = await response.text();
+        const plugins_list = yaml.safeLoad(data);
+        // console.log(plugins_list)
+
+        this.setState({ plugins_list })
+    }
+
+    saveConfig = () => {
+        
+        const values = this.formData()
+
+        // console.log("==>", values)
+        // console.log("======>", yaml.safeDump(values))
+
+        // TODO 失败和成功应该有反馈
+        fetch(api_host + '/bard/config/update', {
+            method: 'POST',
+            body: yaml.safeDump(values),
+        }).then((response) => {
+            // 成功了就干嘛干嘛的
+            // console.log(response)
+            return response.json()
+        },(error) => {
+            message.error('数据传送发生错误[1]');
+        }).then((data) => {
+            switch(data.code) {
+                case 0: message.success('保存配置成功');break;
+                case 1: message.error('无法读取到保存数据');break;
+                case 2: message.error('无法读取到配置文件信息');break;
+                default: message.error('发生其他错误')
+            }
+        }, (error) => {
+            message.error('回复消息出错，无法解析')
+        }).catch((error) => {
+            message.error('数据传送发生错误[2]')
+        })
+    }
+
+    formData = () => {
+        // 形成数据并返回
+        let config = this.state.config
+
+        this.props.form.validateFields((errors, values) => {
+            if(errors) {
+                // 存在错误 提示
+                message.error('输入存在错误');
+            }
+            // console.log(values)
+            // 这里还是手动组装传输数据
+            config.server = values.server
+            config.server_port = values.server_port
+            config.local_address = values.local_address
+            config.local_port = values.local_port
+            config.users[0].username = values.username
+            config.users[0].password = values.password
+            config.users[0].com_config.plugins = values.plugins
+            config.users[0].com_config.TCSP = values.TCSP
+        })
+
+        return config
+    }
+
+    do = (uri, tips1, tips2, tips3) => {
+        fetch(api_host + uri, {
+            method: 'POST',
+        }).then((response) => {
+            return response.json()
+        }, err => {
+            message.error(tips1)
+        }).then(data => {
+            if(data.code === 0) {
+                // ==0 无错
+                message.success(data.message)
+            } else {
+                message.error(data.message)
+            }
+        }, error => {
+            // go程序错误，非shell程序错误
+            message.error(tips2)
+        }).catch( err => {
+            message.error(tips3)
+        })
+    }
+
+    // 打开bard
+    open = () => {
+        this.do(
+            '/bard/start', 
+            "数据接收错误， bard是否打开未知",
+            "打开bard失败",
+            "数据发送失败，打开bard失败",
+        )
+    }
+
+    // 关闭bard
+    close = () => {
+        this.do(
+            '/bard/stop', 
+            "数据接收错误， bard是否关闭未知",
+            "关闭bard失败",
+            "数据发送失败，关闭bard失败",
+        )
+    }
+
+    // 重启bard
+    restart = () => {
+        this.do(
+            '/bard/restart', 
+            "数据接收错误， bard是否打开未知",
+            "重新启动bard失败",
+            "数据发送失败，重新启动bard失败",
+        )
+    }
+
+    // TODO 查看bard的运行状态
+    status = () => {
+
+    }
+
+    // 安装bard
+    install = () => {
+        this.do(
+            '/bard/install', 
+            "数据接收错误， bard是否安装未知未知",
+            "安装bard失败",
+            "数据发送失败，安装bard失败",
+        )
+    }
 
     render() {
 
@@ -41,14 +176,19 @@ class BardConfigForm extends React.Component {
             },
         };
 
-        const { getFieldDecorator } = this.props.form;
+        const { getFieldDecorator, setFieldsValue } = this.props.form;
+
+        const {
+            plugins: plugins_list = [],
+            TCSP: TCSP_list = [],
+        } = this.state.plugins_list || {}
 
         const {
             server,
             server_port,
             local_address,
             local_port,
-            users,
+            users=[],
         } = this.state.config || {}
 
         const {
@@ -77,7 +217,11 @@ class BardConfigForm extends React.Component {
                     {getFieldDecorator('server', {
                         initialValue: server,
                         rules: [{ required: true, message: 'Please input your server ip or domian!', whitespace: true }],
-                    })(<Input.Password />)}
+                    })(<Input.Password 
+                        onChange={v => {
+                            setFieldsValue({ server: v })
+                        }}
+                    />)}
                 </Item>
 
                 <Item
@@ -92,8 +236,14 @@ class BardConfigForm extends React.Component {
                 >
                     {getFieldDecorator('server_port', {
                         initialValue: server_port,
-                        rules: [{ required: true, message: 'Please input your server port!', whitespace: true }],
-                    })(<Input />)}
+                        rules: [{ required: true, message: 'Please input your server port!' }],
+                    })(<InputNumber
+                        style={{ width: '100%' }}
+                        min={1} max={65535}
+                        onChange={v => {
+                            setFieldsValue({ server_port: v })
+                        }} />
+                    )}
                 </Item>
 
                 <Item
@@ -109,7 +259,11 @@ class BardConfigForm extends React.Component {
                     {getFieldDecorator('username', {
                         initialValue: username,
                         rules: [{ required: true, message: 'Please input your username!', whitespace: true }],
-                    })(<Input />)}
+                    })(<Input 
+                        onChange={v => {
+                            setFieldsValue({ username: v })
+                        }}
+                    />)}
                 </Item>
 
                 <Item
@@ -125,7 +279,11 @@ class BardConfigForm extends React.Component {
                     {getFieldDecorator('password', {
                         initialValue: password,
                         rules: [{ required: true, message: 'Please input your password!', whitespace: true }],
-                    })(<Input.Password />)}
+                    })(<Input.Password 
+                        onChange={v => {
+                            setFieldsValue({ password: v })
+                        }}
+                    />)}
                 </Item>
 
 
@@ -142,7 +300,11 @@ class BardConfigForm extends React.Component {
                     {getFieldDecorator('local_address', {
                         initialValue: local_address,
                         rules: [{ required: true, message: 'Please input your local ip!', whitespace: true }],
-                    })(<Input />)}
+                    })(<Input 
+                        onChange={v => {
+                            setFieldsValue({ local_address: v })
+                        }}
+                    />)}
                 </Item>
 
                 <Item
@@ -157,8 +319,16 @@ class BardConfigForm extends React.Component {
                 >
                     {getFieldDecorator('local_port', {
                         initialValue: local_port,
-                        rules: [{ required: true, message: 'Please input your local port!', whitespace: true }],
-                    })(<Input />)}
+                        rules: [{ required: true, message: 'Please input your local port!' }],
+                    })(
+                        <InputNumber
+                            style={{ width: '100%' }}
+                            min={1} max={65535}
+                            onChange={v => {
+                                setFieldsValue({ local_port: v })
+                            }}
+                        />
+                    )}
                 </Item>
 
                 <Item
@@ -173,8 +343,19 @@ class BardConfigForm extends React.Component {
                 >
                     {getFieldDecorator('plugins', {
                         initialValue: plugins,
-                        rules: [{ required: true, message: 'Please enter the plug-in for communication!', whitespace: true }],
-                    })(<Input />)}
+                        // rules: [{ required: true, message: 'Please enter the plug-in for communication!', whitespace: true }],
+                    })(<Select
+                        mode="multiple"
+                        placeholder="Please select"
+                        onChange={(v)=>{
+                            // console.log(v)
+                            setFieldsValue({ plugins: v })
+                        }}
+                      >
+                        {plugins_list.map((v, i) => (
+                            <Option key={i} value={v}>{v}</Option>
+                        ))}
+                      </Select>)}
                 </Item>
 
                 <Item
@@ -189,28 +370,46 @@ class BardConfigForm extends React.Component {
                 >
                     {getFieldDecorator('TCSP', {
                         initialValue: TCSP,
-                        rules: [{ required: true, message: 'Please input your SubProtocol!', whitespace: true }],
-                    })(<Input />)}
+                        // TODO 应该要在用户输入plugins之后限定rules
+                        // rules: [{ required: true, message: 'Please input your SubProtocol!', whitespace: true }],
+                    })(<Select
+                        showSearch
+                        placeholder="Select a TCSP"
+                        optionFilterProp="children"
+                        allowClear
+                        onChange={(v) => {
+                            setFieldsValue({ TCSP: v })
+                        }}
+                        filterOption={(input, option) =>
+                          option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                        }
+                      >
+                          {
+                              TCSP_list.map((v, i) => (
+                                  <Option value={v} key={i}>{v}</Option>
+                              ))
+                          }
+                      </Select>)}
                 </Item>
 
                 <Row style={{ paddingBottom: 8}}>
                     <Col lg={{span: 5, offset: 4}}>
-                        <Button type="primary" htmlType="submit">
+                        <Button type="primary" htmlType="submit" onClick={this.saveConfig}>
                             保存配置
                         </Button>
                     </Col>
                     <Col span={5}>
-                        <Button type="primary" htmlType="submit">
+                        <Button type="primary" htmlType="submit" onClick={this.open}>
                             打开bard
                         </Button> 
                     </Col>
                     <Col span={5}>
-                        <Button type="primary" htmlType="submit">
+                        <Button type="primary" htmlType="submit" onClick={this.close}>
                             关闭bard
                         </Button>
                     </Col>
                     <Col span={5}>
-                        <Button type="primary" htmlType="submit">
+                        <Button type="primary" htmlType="submit" onClick={this.restart}>
                             重启bard
                         </Button>
                     </Col>
